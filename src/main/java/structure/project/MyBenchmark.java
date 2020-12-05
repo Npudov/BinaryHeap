7bfa32fe69087f7bf3f486c98ece050df31f98bb
somagic2 <nik.p008@gmail.com>
2020-12-05 18:50:45 +0300

    add benchmark tests

--- src/main/java/structure/project/MyBenchmark.java
+++ src/main/java/structure/project/MyBenchmark.java
@@ -8,21 +8,26 @@ import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @State(Scope.Benchmark)
-@Fork(value = 1, warmups = 1)
-@Warmup(iterations = 1, time = 300, timeUnit = TimeUnit.MILLISECONDS)
-@Measurement(iterations = 5, time = 100, timeUnit = TimeUnit.MILLISECONDS)
+@Fork(value = 1, jvmArgs = {"-Xms2G", "-Xmx2G"})
+@Warmup(iterations = 3 ,time = 300, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 5, time = 500, timeUnit = TimeUnit.MILLISECONDS)
 
 public class MyBenchmark {
     @Param({"1000"})
     private int N;
 
     private BinaryHeap binaryHeap = new BinaryHeap();
+    private List<Integer> list = new ArrayList<>();
+    final Random random = new Random();
 
     public static void main(String[]args) throws RunnerException {
 
@@ -37,14 +42,25 @@ public class MyBenchmark {
     @Setup
     public void setup() {
         binaryHeap = createData();
+        list = createList();
     }
 
     @Benchmark
     public void add(Blackhole bh) {
-        int listSize = binaryHeap.getHeap().size();
-        for (int i = 0; i < listSize; i++) {
+        int size = binaryHeap.getHeap().size();
+        for (int i = 0; i < size; i++) {
             Integer element = binaryHeap.getHeap().get(i);
             binaryHeap.adder(i);
+            bh.consume(element);
+        }
+    }
+
+    @Benchmark
+    public void addInList(Blackhole bh) {
+        int listSize = list.size();
+        for (int i = 0; i < listSize; i++) {
+            Integer element = list.get(i);
+            list.add(i);
             bh.consume(element);
         }
     }
@@ -52,9 +68,17 @@ public class MyBenchmark {
     private BinaryHeap createData() {
         BinaryHeap tempBinaryHeap = new BinaryHeap();
         for (int i = 0; i < N; i++) {
-            tempBinaryHeap.adder(i);
+            tempBinaryHeap.adder(random.nextInt(1000 + 1));
         }
         return tempBinaryHeap;
+    }
+
+    private List<Integer> createList() {
+        List<Integer> tempList = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            tempList.add(random.nextInt(1000 + 1));
+        }
+        return tempList;
     }
 
 }
